3384022a170e968e54a5c0bedf39cbbe8a6d3d87
Diyaaa <diyaabhisharma@gmail.com>
2025-06-13 10:09:22 +0530

    commit changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,18 @@
+// import mongoose from "mongoose";
+
+// export const connectDB = async () => {
+//     await mongoose.connect('mongodb+srv://diyaabhisharma:13251294@cluster0.sonho.mongodb.net/Dineoo').then(()=>console.log("DB Connected"));
+// }
+
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://diyaabhisharma:13251294@cluster0.sonho.mongodb.net/Dineoo').then(()=>console.log("DB Connected"));
-}
+  try {
+    await mongoose.connect(
+      "mongodb+srv://diyaabhisharma:13251294@cluster0.mongodb.net/Dineoo?retryWrites=true&w=majority"
+    );
+    console.log("DB Connected");
+  } catch (error) {
+    console.error("DB connection failed:", error.message);
+  }
+};
